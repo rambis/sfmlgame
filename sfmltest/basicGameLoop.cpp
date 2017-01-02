c6dd7916165a0ef4a0f18dee442a3238cb88033d
Sriram Ananth <sananth@idirect.net>
2017-01-01 23:13:17 -0500

    First test of textures

--- sfmltest/basicGameLoop.cpp
+++ sfmltest/basicGameLoop.cpp
@@ -1,46 +1,67 @@
 #include <SFML/Graphics.hpp>
 #include <windows.h>
+#include <stdio.h>
+#include <string>
 #include "globalHeader.h"
 
 void basicGameLoop() // Just basic events using keyboards and mouse
 {
-	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!"); // Opens a windows of size 500 x 500 
-
 	float rad = 50.f;	// Radius of circle
 	float xPos = 0;		// X-coordinates of circle
 	float yPos = 0;		// Y-coordinates of circle
 
+	sf::RenderWindow window(sf::VideoMode(500, 500), "SFML works!"); // Opens a windows of size 500 x 500 
+
+	sf::CircleShape shape(rad); // create an object of circleshape
+//	shape.setFillColor(sf::Color::Blue);
+
+	char path[MAX_PATH];
+	GetCurrentDirectoryA(MAX_PATH, path);
+	strcat_s(path, "\\Resources\\");
+	strcat_s(path, "skyTxt.jpg");
+	printf("%s\n", path);
+
 	sf::Texture texture;
-	if (!texture.loadFromFile("sakyTxt.jpg")) {
-		sf::Text text;
-		sf::Font font;
-		text.setFont(font);
-		text.setString("Error loading picture");
-		text.setFillColor(sf::Color::White);
-		text.setCharacterSize(40);
-		//window.clear();	
-		window.draw(text);
-		window.display();
-		Sleep(10000);
-		window.close();
+	if (!texture.loadFromFile(path)) {
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed) {
+					window.close();
+				}
+			}
+
+			sf::Text text;
+			sf::Font font;
+
+			if (!font.loadFromFile("OpenSans-Bold.ttf"))
+				return;
+			text.setFont(font);
+			text.setString("Error loading picture");
+			text.setFillColor(sf::Color::Black);
+			text.setCharacterSize(40);
+
+			window.clear(sf::Color::White);
+			window.draw(text);
+			window.display();
+		}
 	}
 		
 
-	sf::CircleShape shape(rad); // create an object of circleshape
-	shape.setFillColor(sf::Color::Blue);
+
 
 	//window.setKeyRepeatEnabled(false); // Used to implement key de-bouncing
 
 	while (window.isOpen()) // Main 'game' loop as it's called
 	{
 		sf::Event event;
-		sf::Text text;
-		text.setString("Error loading picture");
-		text.setFillColor(sf::Color::White);
-		text.setCharacterSize(40);
-		window.clear();
-		window.draw(text);
-		window.display();
+		sf::Sprite sprite;
+		sprite.setTexture(texture);
+		texture.setSmooth(true);
+		sprite.scale(sf::Vector2f(1.f, 1.f));
+		sprite.setTextureRect(sf::IntRect(50, 50, 500, 500));
 
 		while (window.pollEvent(event)) // Poll event. Don't use this for real-time games. It isn't good for realtime applications. Just testing here.
 		{
@@ -70,6 +91,7 @@ void basicGameLoop() // Just basic events using keyboards and mouse
 					sf::Vector2i k = sf::Mouse::getPosition(window);	// getPositon returns an object of Verctor2i class. It's basic structure is (x, y) like a pair.
 					xPos = k.x - rad;
 					yPos = k.y - rad;
+				
 					shape.setPosition(xPos, yPos); //set position of circle on screen.
 				}
 				break;
@@ -91,7 +113,7 @@ void basicGameLoop() // Just basic events using keyboards and mouse
 			rad = 0;
 
 		window.clear();	// Always required in a game loop. Otherwise, pixels from previous frame will remain on the screen.
-		window.draw(shape); // Drawing basically puts all the pixels into a hidden buffer behind the scenes
+		window.draw(sprite); // Drawing basically puts all the pixels into a hidden buffer behind the scenes
 		window.display(); // Reads from the hidden buffer and displays onto screen
 	}
 }
