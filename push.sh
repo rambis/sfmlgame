99a900634f6bee30db9a0a6df6190721ca66221a
Sriram Ananth <hotocheem@gmail.com>
2017-01-09 00:00:24 -0500

    Vertex Array half done

--- push.sh
+++ push.sh
@@ -6,15 +6,15 @@ git status
 #echo
 #if [[ $REPLY =~ ^[Yy]$ ]]
 #then
-	git add .
-	read -p "Commit Message: " commitmsg
-	echo
-	git commit -m "$commitmsg"
-	echo "Committed! Pushing now"
-	echo
-	git push http://github.com/rambis/sfmlgame
+git add .
+read -p "Commit Message: " commitmsg
+echo
+git commit -m "$commitmsg"
+echo "Committed! Pushing now"
+echo
+git push http://github.com/rambis/sfmlgame
 #else
-	exit 0
+exit 0
 
 #fi	
 
--- sfmltest/characterMove.cpp
+++ sfmltest/characterMove.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include "globalHeader.h"
+#include <stdlib.h> 
 
 // Move character sprites across the screen
 void characterMove() 
@@ -13,11 +14,13 @@ void characterMove()
 	sf::Vector2i source;
 	sf::Vector2f position(0, 0); // Source -> keyboard input, position -> sprite position on window
 
-	sf::VideoMode getScrRes = sf::VideoMode::getDesktopMode();
+//	sf::Vector2f screenRes((float)getScrRes.width, (float)getScrRes.height);
 
-	sf::Vector2f screenRes((float)getScrRes.width, (float)getScrRes.height);
-
-	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "HELLO!");
+	sf::VideoMode videoMode(sf::VideoMode::getDesktopMode());
+	sf::Vector2i screenRes;
+	screenRes.x = videoMode.width;
+	screenRes.y = videoMode.height;
+	sf::RenderWindow window(videoMode, "HELLO!");
 
 	sf::Event event;
 
@@ -55,51 +58,92 @@ void characterMove()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			window.close();
 
 		myPlayer.setTextureRect(sf::IntRect((int)(150 * source.x), 117 * source.y, 137, 117));
 
 
-		switch (event.key.code)
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 		{
-		case sf::Keyboard::Down:
 			source.y = DOWN;
-			if (event.type == sf::Event::KeyPressed)
-				position.y++;
-			break;
+			/*if (event.type == sf::Event::KeyPressed)
+				position.y++;*/
+			myPlayer.move(0, 1);
+		}
 
-		case sf::Keyboard::Up:
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
 			source.y = UP;
-			if (event.type == sf::Event::KeyPressed)
-				position.y--;
-			break;
+			/*if (event.type == sf::Event::KeyPressed)
+				position.y--;*/
+			myPlayer.move(0, -1);
+		}
 
-		case sf::Keyboard::Left:
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
 			source.y = LEFT;
-			if (event.type == sf::Event::KeyPressed)
-				position.x--;
-			break;
+			/*if (event.type == sf::Event::KeyPressed)
+				position.x--;*/
+			myPlayer.move(-1, 0);
+		}
 
-		case sf::Keyboard::Right:
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
 			source.y = RIGHT;
-			if (event.type == sf::Event::KeyPressed)
-				position.x++;
-			break;
+			/*if (event.type == sf::Event::KeyPressed)
+				position.x++;*/
+			myPlayer.move(1, 0);
 		}
 
 		sf::Time elapsed = clock.getElapsedTime();
+		std::cout << elapsed.asSeconds() << std::endl;
+	//	myPlayer.setPosition(position);
 
-		myPlayer.setPosition(position);
-
-		if ((event.type == sf::Event::KeyPressed) & (elapsed.asMilliseconds() % 500 == 0))
+		if ((event.type == sf::Event::KeyPressed) & (elapsed.asMilliseconds() > 50))
+		{
 			source.x++;
-		else
+			clock.restart();
+		}
+		
+		if(event.type == sf::Event::KeyReleased)
 			source.x = 0;
 
 		if (source.x > 6)
 			source.x = 0;
 
+		// Vertex array
+		sf::VertexArray vArray(sf::PrimitiveType::Quads, 4);
+		sf::Vector2i arrayBlockSize(10, 10);
+
+		for (int i = 0; i < (screenRes.x / arrayBlockSize.x); i++)
+		{
+			for (int j = 0; j < screenRes.y / arrayBlockSize.y; j++)
+			{
+				/*vArray[0].position = sf::Vector2f(i * arrayBlockSize.x, j * arrayBlockSize.y);
+				vArray[1].position = sf::Vector2f((i + 1)* arrayBlockSize.x, j * arrayBlockSize.y);
+				vArray[2].position = sf::Vector2f((i + 1) * arrayBlockSize.x, (j + 1) * arrayBlockSize.y);
+				vArray[3].position = sf::Vector2f(i * arrayBlockSize.x, (j + 1) * arrayBlockSize.y);*/
+
+				vArray[0].position = sf::Vector2f(10,10);
+				vArray[1].position = sf::Vector2f(10,200);
+				vArray[2].position = sf::Vector2f(200,200);
+				vArray[3].position = sf::Vector2f(200,10);
+
+				for (int k = 0; k < 4; k++)
+				{
+					int red = rand() % 255;
+					int green = rand() % 255;
+					int blue = rand() % 255;
+
+					vArray[k].color = sf::Color(red, green, blue);
+				}
+			}
+		}
+
 		window.clear();
-		window.draw(bgSprite);
+		window.draw(vArray);
+//		window.draw(bgSprite);
 		window.draw(myPlayer);
 		window.display();
 	}
